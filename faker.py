0eac2de25df0ddb34512c2a7dbc2c608d65c8cc1
artempelevin <artempelevin@list.ru>
2025-03-09 17:11:36 +0300

    fix: правильно определяем смену знаков функции (самое точное - это определять в момент генерации сигнала)

--- faker.py
+++ faker.py
@@ -28,7 +28,11 @@ def generate_parabola(
     return x, y
 
 
-def generate_acoustic_signal() -> tuple[np.ndarray, np.ndarray]:
+def generate_acoustic_signal() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Генерирует "фейковый" акустический сигнал удара молотка по бетонной свае
+    :return: x, y координаты функции, изображающей сигнал и нулевые точки x, в которых функция меняет знак
+    """
     segments = np.random.randint(15, 20)
     y_coefficient = 1.0
     cur_width = np.random.uniform(2, 5)
@@ -37,6 +41,8 @@ def generate_acoustic_signal() -> tuple[np.ndarray, np.ndarray]:
 
     x_all = []
     y_all = []
+    zero_crossings = []
+    last_sign = "plus"
 
     for segment in range(1, segments + 1):
         cur_y *= y_coefficient - (segment / segments)
@@ -44,46 +50,26 @@ def generate_acoustic_signal() -> tuple[np.ndarray, np.ndarray]:
         x, y = generate_parabola(vertex=(cur_x, cur_y), width=cur_width, num_points=100)
         if np.random.random() < 0.5:  # Разворачиваем ветви параболы с вероятностью 50%
             y = -y
+            cur_sign = "minus"
+        else:
+            cur_sign = "plus"
 
         x_all.append(x)
         y_all.append(y)
+        if cur_sign != last_sign:
+            zero_crossings.append(cur_x-cur_width)
 
         new_width = cur_width + cur_width * 0.07
         cur_x = cur_x + new_width + cur_width
         cur_width = new_width
+        last_sign = cur_sign
 
-    return np.concatenate(x_all), np.concatenate(y_all)
-
-
-def find_zero_crossings(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """
-    Находит точки на оси x, где y меняет знак.
-
-    Аргументы:
-        x: np.ndarray - массив значений аргумента.
-        y: np.ndarray - массив значений функции.
-
-    Возвращает:
-        np.ndarray с рассчитанными значениями x, где знак функции изменяется на нулевой.
-    """
-    assert len(x.shape) == len(y.shape) == 1, f"Shape должен быть (n, ), но получено: {x.shape}, {y.shape}"
-    assert x.shape == y.shape, f"x.shape ({x.shape}) != y.shape ({y.shape})."
-
-    zero_points = []
-    for i in range(len(y) - 1):
-        if y[i] * y[i + 1] < 0:  # если сменился знак
-            # Интерполяция: y = y[i] + t*(y[i+1] - y[i]) = 0, находим t
-            t = -y[i] / (y[i + 1] - y[i])
-            # Вычисляем соответствующее x
-            x_zero = x[i] + t * (x[i + 1] - x[i])
-            zero_points.append(x_zero)
-    return np.array(zero_points)
+    return np.concatenate(x_all), np.concatenate(y_all), np.array(zero_crossings)
 
 
-x, y = generate_acoustic_signal()
+x, y, zero_crossings_xs = generate_acoustic_signal()
 
 plt.plot(x, y, "black")
-zero_crossings_xs = find_zero_crossings(x, y)
 for zero_crossings_x in zero_crossings_xs:
     plt.axvline(x=zero_crossings_x, color='red', linestyle='dotted')
 
