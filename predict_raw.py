e84c2ac7e0d34b5fa85884643297944319c3c806
artempelevin <artempelevin@list.ru>
2025-05-02 16:04:19 +0300

    refactor: рефакторинг predict.raw.py

--- predict_raw.py
+++ predict_raw.py
@@ -4,7 +4,7 @@ import numpy as np
 from keras import models, Sequential
 from matplotlib import pyplot as plt
 
-from utils import draw_acoustic_signal, draw_level_lines, get_generator_function_by_model_number
+from utils import draw_acoustic_signal, get_generator_function_by_model_number
 
 
 def prediction(model_number: int, weights_path: str) -> None:
@@ -17,26 +17,20 @@ def prediction(model_number: int, weights_path: str) -> None:
         x, y, start_x, reflection_x = generate_pulse_signal()
 
         model_input = np.column_stack((x, y)).reshape(-1)  # делаем массив точек формата [x1, y1, x2, y2, ..., xn, yn]
-        predict = model.predict(np.array([model_input]))[0]
+        predict = model.predict(np.array([model_input]), verbose=0)[0]
         start_x_predict, reflection_x_predict = predict
 
+        mae_1_point = abs(start_x - start_x_predict)
+        mae_2_point = abs(reflection_x - reflection_x_predict)
+
         fig, ax = plt.subplots()
-
-        print(f"MAE точки начала: {abs(start_x - start_x_predict)}")
-        print(f"MAE таки отражения: {abs(reflection_x - reflection_x_predict)}")
+        plt.title(f"1 point mae = {mae_1_point}\n2 point mae = {mae_2_point}")
         draw_acoustic_signal(ax=ax, x=x, y=y)
-        draw_level_lines(
-            ax=ax, start_x=start_x, reflection_x=reflection_x, color="blue", linestyle="dashdot", alpha=0.5
-        )
-        draw_level_lines(
-            ax=ax,
-            start_x=start_x_predict,
-            reflection_x=reflection_x_predict,
-            color="red",
-            linestyle="dashdot",
-            alpha=0.5,
-        )
-
+        ax.axvline(x=start_x, color="c", linestyle="dashdot", alpha=0.5, label="1 point (true label)")
+        ax.axvline(x=start_x_predict, color="m", linestyle="dashdot", alpha=0.5, label="1 point (model predict)")
+        ax.axvline(x=reflection_x, color="c", linestyle="dashdot", alpha=0.5, label="2 point (true label)")
+        ax.axvline(x=reflection_x_predict, color="m", linestyle="dashdot", alpha=0.5, label="2 point (model predict)")
+        plt.legend()
         plt.show()
 
 
